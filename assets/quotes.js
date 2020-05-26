d7d41e0c8622311b785054426e74a3a9a96fe76a
pglc0956 <64751147+pglc0956@users.noreply.github.com>
2020-05-26 09:55:42 +0800

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -4,7 +4,7 @@ export default [
     { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
     { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
     { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "standinshd", quotes: "I may not with ysssou evveryday, but I love you everyday." },
     { author: "standinshd", quotes: "I love you." },
     { author: "standinshd", quotes: "I'm sorry for loving you." },
 ];
